a34f3c5e24772feda9f18636e0e10fe9b459bf1d
Valeryia Slepianok <direnok.vd@mail.ru>
2022-03-09 18:00:50 +0300

    refactoring

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,24 @@
-const slider = tns({
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log(1);
+
+  const slider = tns({
     container: '.slider',
     items: 3,
     slideBy: 1,
-    autoplay: true,
-    autoplay: true,
+   autoplay: true, 
     autoplayTimeout: 1800,
     controls: false,
     navPosition: 'bottom',
     navAsThumbnails: true,
     arrowKeys: true,
-   autoplayButtonOutput: false, 
+    autoplayButtonOutput: false,
   });
-
+  
   document.querySelector('.prev').addEventListener('click', () => {
     slider.goTo('prev');
   });
-
+  
   document.querySelector('.next').addEventListener('click', () => {
     slider.goTo('next');
   });
+});
