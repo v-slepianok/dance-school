5b959a8db87199fdede558bfcb77aef4496c1533
Valeryia Slepianok <direnok.vd@mail.ru>
2022-03-11 19:15:11 +0300

    adapted design for various devices

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log(1);
 
   const slider = tns({
     container: '.slider',
@@ -12,6 +11,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
     navAsThumbnails: true,
     arrowKeys: true,
     autoplayButtonOutput: false,
+    responsive: {
+      320: {
+        items: 1
+      },
+      684: {
+        items: 2
+      },
+      840: {
+        items: 3
+      }
+    }
   });
   
   document.querySelector('.prev').addEventListener('click', () => {
@@ -21,4 +31,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.querySelector('.next').addEventListener('click', () => {
     slider.goTo('next');
   });
+
+//hamburger menu
+  const menu = document.querySelector('.nav'),
+    menuItem = document.querySelectorAll('.nav_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+      console.log('1');
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('nav_active');
+
+    menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      nav.classList.toggle('nav_active');
+      })
+    })
+  });
+
+// btn read more
+  const readMoreBtn = document.getElementById('read_more');
+  const textMobile = document.getElementById('aboutus-description-mobile');
+  const textFull = document.getElementById('aboutus-description-full');
+
+  readMoreBtn.addEventListener('click', () => {
+    textFull.style.display = 'block';
+    textMobile.style.display = 'none';
+  });
+
 });
